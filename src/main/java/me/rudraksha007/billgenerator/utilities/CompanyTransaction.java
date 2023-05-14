0a4013cbe4afd136d60b951f9670d4d344a2211c
rudr <rudraman.2805@gmail.com>
2023-05-14 12:11:17 +0530

    summary progress

---
+++ src/main/java/me/rudraksha007/billgenerator/utilities/CompanyTransaction.java
@@ -0,0 +1,9 @@
+package me.rudraksha007.billgenerator.utilities;
+
+import java.util.Date;
+
+public class CompanyTransaction {
+    private String supplier;
+    private Date date;
+    
+}
---
+++ src/main/java/me/rudraksha007/billgenerator/utilities/SQLCommands.java
@@ -0,0 +1,7 @@
+package me.rudraksha007.billgenerator.utilities;
+
+public class SQLCommands {
+
+    public static String ADD = "INSERT INTO";
+    public static String 
+}
