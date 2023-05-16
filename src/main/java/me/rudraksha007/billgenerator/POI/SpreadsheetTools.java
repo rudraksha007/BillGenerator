88b5ce5537263888e5dd032f995bd60069fbf2ac
rudr <rudraman.2805@gmail.com>
2023-05-16 17:17:09 +0530

    Summary done

--- src/main/java/me/rudraksha007/billgenerator/POI/SpreadsheetTools.java
+++ src/main/java/me/rudraksha007/billgenerator/POI/SpreadsheetTools.java
@@ -16,41 +16,47 @@ import java.io.IOException;
 import java.util.Vector;
 
 public class SpreadsheetTools {
-    File head = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()+File.separator+"" +
-            "BillHeader.xlsx");
-    File foot = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()+File.separator+"" +
-            "BillFooter.xlsx");
+    File template = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()+File.separator+
+            "BillTemplate.xlsx");
 
+//    @SuppressWarnings("all")
     public void saveXLSX(Vector<Vector> data){
-        System.out.println(head.getAbsolutePath());
         File toSave = new File(DataManager.getDataFolder()+("Exports/exported.xlsx").replace("/", File.separator));
         try {
             System.out.println(toSave.getAbsolutePath());
+            Workbook workbook;
             if (!toSave.exists()) {
                 if (!toSave.getParentFile().exists())toSave.getParentFile().mkdir();
                 toSave.createNewFile();
+                workbook = new XSSFWorkbook();
             }
-            Workbook workbook = new XSSFWorkbook();
-            Sheet sheet = workbook.createSheet(String.valueOf(workbook.getNumberOfSheets()+1));
-//            pasteHeader(sheet);
-//            pasteData(sheet, data);
-            pasteFooter(sheet, Math.max(15, data.size()));
+            else workbook = new XSSFWorkbook(toSave);
+            Sheet sheet;
+            int i = 0;
+            while ((data.size()-15*i)>15){
+                if (data.size()>15)sheet = workbook.createSheet((workbook.getNumberOfSheets() + 1 )+"-"+i);
+                else sheet = workbook.createSheet(String.valueOf((workbook.getNumberOfSheets() + 1 )));
+                pasteTemplate(sheet);
+                pasteData(sheet, data, i);
+                i++;
+            }
             FileOutputStream stream = new FileOutputStream(toSave);
             workbook.write(stream);
             workbook.close();
             stream.flush();
             stream.close();
-        } catch (IOException e) {
+        } catch (IOException | InvalidFormatException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    public void pasteHeader(Sheet dest){
+    public void pasteTemplate(Sheet dest){
         try {
-            Workbook source = new XSSFWorkbook(head);
-            Sheet sourceSheet = source.getSheetAt(0);
-            for (Row row: sourceSheet){
+            Workbook templateBook = new XSSFWorkbook(template);
+            Sheet temp = templateBook.getSheetAt(0);
+            for (CellRangeAddress add: temp.getMergedRegions())dest.addMergedRegion(add);
+            for (Row row: temp){
                 Row destRow = dest.createRow(row.getRowNum());
                 int i = 0;
                 for (Cell cell: row){
@@ -73,52 +79,13 @@ public class SpreadsheetTools {
         }
     }
 
-    public void pasteFooter(Sheet dest, int lastWrittenRow){
-        try {
-            Workbook source = new XSSFWorkbook(foot);
-            Sheet sourceSheet = source.getSheetAt(0);
-            for (Row row: sourceSheet){
-                Row destRow = dest.createRow(row.getRowNum()+lastWrittenRow+1);
-                int i = 0;
-                for (Cell cell: row){
-                    Cell destCell = destRow.createCell(cell.getColumnIndex());
-                    switch (cell.getCellType()){
-                        case NUMERIC:destCell.setCellValue(cell.getNumericCellValue());break;
-                        case FORMULA: destCell.setCellFormula(cell.getCellFormula());break;
-                        default: destCell.setCellValue(cell.getRichStringCellValue());
-                    }
-                    XSSFCellStyle style = (XSSFCellStyle) row.getCell(i).getCellStyle();
-                    if (style==null)continue;
-                    XSSFCellStyle destStyle = (XSSFCellStyle) dest.getWorkbook().createCellStyle();
-                    destStyle.cloneStyleFrom(style);
-                    destCell.setCellStyle(destStyle);
-                    i++;
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InvalidFormatException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void pasteData(Sheet sheet, Vector<Vector> data){
-        sheet.getRow(0).getCell(0).setCellValue(1);
-        sheet.getRow(0).getCell(1).setCellValue(String.valueOf(data.get(0).get(1)));
-        sheet.getRow(0).getCell(7).setCellValue(String.valueOf(data.get(0).get(2)));
-        sheet.getRow(0).getCell(10).setCellValue(Double.parseDouble(String.valueOf(data.get(0).get(3))));
-        sheet.getRow(0).getCell(11).setCellValue(Double.parseDouble(String.valueOf(data.get(0).get(4)).split("/")[0]));
-        for (int row = 1; row < data.size(); row++) {
-            sheet.getRow(row).setRowStyle(sheet.getRow(0).getRowStyle());
-            Row r = sheet.createRow(row+22);
-            r.createCell(0).setCellValue(1+row);
-            r.createCell(1).setCellValue(String.valueOf(data.get(row).get(1)));
-            r.createCell(7).setCellValue(String.valueOf(data.get(row).get(2)));
-            r.createCell(10).setCellValue(Integer.parseInt(String.valueOf(data.get(row).get(3))));
-            r.createCell(11).setCellValue(Integer.parseInt(String.valueOf(data.get(row).get(4)).split("/")[0]));
-            sheet.addMergedRegion(new CellRangeAddress(row+22, row+22, 1,6));
-            sheet.addMergedRegion(new CellRangeAddress(row+22, row+22, 7,9));
-            sheet.addMergedRegion(new CellRangeAddress(row+22, row+22, 11,14));
+    public void pasteData(Sheet sheet, Vector<Vector> data, int sheetNo){
+        for (int i = 22; i < 36; i++) {
+            Row row = sheet.getRow(i);
+            row.getCell(1).setCellValue(String.valueOf(data.get(i).get(1+15*sheetNo)));
+            row.getCell(2).setCellValue(String.valueOf(data.get(i).get(2+15*sheetNo)));
+            row.getCell(3).setCellValue(Integer.parseInt(String.valueOf(data.get(i).get(3+15*sheetNo))));
+            row.getCell(4).setCellValue(Float.parseFloat(String.valueOf(data.get(i).get(4+15*sheetNo)).split("/")[0]));
         }
     }
 }
